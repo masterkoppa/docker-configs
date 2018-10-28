c66d0b9e82917bc58334eb8d7dcc9b8f1e053b19
Andres Ruiz <andresruiz2010@gmail.com>
2018-10-27 22:46:35 -0400

    Fix syntax issue in build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ for i in $(find . -type d)
 do
     # Iterate over every folder
     if [ -f "$i/Dockerfile" ]; then
-        cd $i && docker-compose config > /dev/null && docker-compose build && cd ..
-    elif [ -f "$i/docker-compose.yaml" ]
-        cd $i && docker-compose config > /dev/null && docker-compose pull && cd ..
+        cd $i && docker-compose config > /dev/null && docker-compose build && cd ..;
+    elif [ -f "$i/docker-compose.yaml" ]; then
+        cd $i && docker-compose config > /dev/null && docker-compose pull && cd ..;
     fi
 done
